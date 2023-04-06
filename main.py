6ca1ea01f21fd7960882e51263c0e1ff6340b0a0
Kakise <me@kakise.tech>
2023-04-06 17:22:51 +0200

    swtched to azure openai, slightly better prompts and other ui/ux enhancements

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask, url_for, session, request
-from flask import render_template, redirect
+import os
+
 from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv
-
-import os
+from flask import Flask, redirect, render_template, request, session, url_for
 
 from news import News, NewsFeed
 
@@ -11,48 +10,80 @@ load_dotenv(".env.dev")
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", os.urandom(24))
-app.config.from_object('config')
+app.config.from_object("config")
 
-CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+CONF_URL = "https://accounts.google.com/.well-known/openid-configuration"
 oauth = OAuth(app)
 oauth.register(
-    name='google',
+    name="google",
     server_metadata_url=CONF_URL,
-    client_kwargs={
-        'scope': 'openid email profile'
-    }
+    client_kwargs={"scope": "openid email profile"},
 )
 
 
-@app.route('/')
+@app.route("/")
 def homepage():
-    user = session.get('user')
+    user = session.get("user")
     newsfeed = NewsFeed().get_all_news()
-    return render_template('home.html', user=user, newsfeed=newsfeed)
+    return render_template("home.html", user=user, newsfeed=newsfeed)
 
-@app.route('/news/add', methods=['POST'])
+
+@app.route("/edit")
+def editpage():
+    user = session.get("user")
+    newsfeed = NewsFeed().get_all_news()
+    return render_template("home.html", user=user, newsfeed=newsfeed, editMode=True)
+
+
+@app.route("/news/add", methods=["POST"])
 def add_news():
-    if not session.get('user') or session.get('user')['email'].split("@")[-1] != "artefact.com":
-        return redirect('/login')
+    if session.get("user") is None:
+        return redirect("/login")
+    if (
+        session.get("user", {"email": "anon@anon.net"})
+        .get("email", "anon@anon.net")
+        .split("@")[-1]
+        != "artefact.com"
+    ):
+        return redirect("/logout")
     # get link from post param
-    news = News(request.form.get("link"), session.get('user'))
-    news.save()
-    return redirect('/')
+    if request.form.get("link") is not None:
+        news = News(request.form.get("link", ""), session.get("user", {}))
+        news.save()
+    return redirect("/")
 
-@app.route('/login')
+
+@app.route("/news/search", methods=["POST"])
+def search_news():
+    if request.form.get("query") is not None:
+        newsfeed = NewsFeed().search(request.form.get("query", ""))
+        return render_template("home.html", newsfeed=newsfeed, user=session.get("user"), query=request.form.get("query", ""))
+    return redirect("/")
+
+
+@app.route("/news/delete", methods=["POST"])
+def delete_news():
+    if session.get("user") is None:
+        return redirect("/login")
+
+    print(NewsFeed().delete(request.form.get("link")))
+    return redirect("/")
+
+
+@app.route("/login")
 def login():
-    redirect_uri = url_for('auth', _external=True)
+    redirect_uri = url_for("auth", _external=True)
     return oauth.google.authorize_redirect(redirect_uri)
 
 
-@app.route('/auth')
+@app.route("/auth")
 def auth():
     token = oauth.google.authorize_access_token()
-    session['user'] = token['userinfo']
-    return redirect('/')
+    session["user"] = token["userinfo"]
+    return redirect("/")
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    session.pop('user', None)
-    return redirect('/')
+    session.pop("user", None)
+    return redirect("/")
--- news.py
+++ news.py
@@ -1,21 +1,28 @@
 import os
+from datetime import datetime
+from typing import Any, Dict, List, NewType
 
 import requests
 from bs4 import BeautifulSoup
-from langchain import LLMChain, OpenAI, PromptTemplate
-from langchain.chains.mapreduce import MapReduceChain
+from langchain import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
 from langchain.docstore.document import Document
+from langchain.llms import AzureOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import CharacterTextSplitter
-from datetime import datetime
-
 from pymongo import MongoClient
+
+NewsType = NewType("NewsType", Dict[str, str])
 
 
 class News:
-    def __init__(self, link: str, user: any = None):
-        llm = OpenAI(temperature=0)
+    def __init__(self, link: str, user: Any):
+        if link is None:
+            raise ValueError("Link cannot be None")
+
+        llm = AzureOpenAI(
+            deployment_name="text-davinci-003", model_name="text-davinci-003"
+        )
         text_splitter = CharacterTextSplitter()
 
         req = requests.get(link)
@@ -30,7 +37,7 @@ class News:
 
         doc = [Document(page_content=t) for t in texts]
 
-        prompt_template = """Write a concise summary of the following:
+        prompt_template = """Write a concise summary, in english, of the following:
 
 
         {text}
@@ -45,31 +52,70 @@ class News:
 
         TITLE IN ENGLISH:"""
         PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
-        PROMPT_TITLE = PromptTemplate(template=prompt_template_title, input_variables=["text"])
+        PROMPT_TITLE = PromptTemplate(
+            template=prompt_template_title, input_variables=["text"]
+        )
 
         chain = load_summarize_chain(llm, chain_type="stuff", prompt=PROMPT)
         chain_title = load_summarize_chain(llm, chain_type="stuff", prompt=PROMPT_TITLE)
         self.title = chain_title.run(doc)
         self.excerpt = chain.run(doc)
         self.link = link
-        self.user = user
+        if user is not None:
+            self.user = user
+        else:
+            self.user = {"name": "Anonymous"}
 
     def save(self):
         client = MongoClient(os.getenv("MONGO_URI"))
         db = client.get_database("news")
         news = db.news
-        news.insert_one({"title": self.title, "excerpt": self.excerpt, "link": self.link, "author": self.user["name"], "date": datetime.now().strftime('%Y-%m-%d')})
+        # Check if news already exists
+        if news.find_one({"link": self.link}) is not None:
+            print("This news is already in the database")
+        else:
+            news.insert_one(
+                {
+                    "title": self.title,
+                    "excerpt": self.excerpt,
+                    "link": self.link,
+                    "author": self.user["name"],
+                    "date": datetime.now().strftime("%Y-%m-%d"),
+                }
+            )
+
 
 class NewsFeed:
     def __init__(self):
-        self.news = []
+        self.news: List[NewsType] = []
         client = MongoClient(os.getenv("MONGO_URI"))
-        db = client.get_database("news")
-        news = db.news
+        self.db = client.get_database("news")
+        news = self.db.news
         for n in news.find():
             self.news.append(n)
-    
+
     def get_one(self):
         return self.news[-1]
+
     def get_all_news(self):
         return self.news[::-1]
+
+    def search(self, text: str) -> List[NewsType]:
+        result = self.db.news.aggregate(
+            [
+                {
+                    "$search": {
+                        "index": "default",
+                        "text": {"query": text, "path": {"wildcard": "*"}},
+                    }
+                }
+            ]
+        )
+        return list(result)
+
+    def delete(self, link: str | None) -> int | None:
+        try:
+            result = self.db.news.delete_one({"link": link})
+            return result.deleted_count
+        except Exception as e:
+            print(e)
